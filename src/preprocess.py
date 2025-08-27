5a4443fd53959d52e43fe0868ff1b7f872fef592
Juan Manuel Carballo <jm.carb@gmail.com>
2025-08-27 17:34:33 -0600

    Fixes in preprocess script

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,25 +1,46 @@
 import os
+import sys
 import glob
+import warnings
+
+warnings.filterwarnings("ignore", module="pylidc", category=UserWarning)
+
+
 import numpy as np
+import pandas as pd
 import pylidc as pl
 import SimpleITK as sitk
-import threading as t
 import concurrent.futures as c
 import matplotlib.pyplot as plt
 
+from tqdm import tqdm
 from PIL import Image
 from pylidc.utils import consensus
 from datasets import Dataset, Image as HFImage
+from collections import deque
 from huggingface_hub import login
 from dotenv import load_dotenv, find_dotenv
+from statistics import median_high
 
 
 load_dotenv(find_dotenv())
+
 
 IMG_MASK = "./data/image_masks"
 PATCH_MASK = "./data/patches_mask"
 IMG = "./data/images"
 PATCH = "./data/patches"
+
+
+class NoP:
+
+    def __enter__(self):
+        self._original_stdout = sys.stdout
+        sys.stdout = open(os.devnull, "w")
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout.close()
+        sys.stdout = self._original_stdout
 
 
 def save_image(arr: np.ndarray, file_path: str, interpolator=sitk.sitkLinear):
@@ -58,9 +79,25 @@ def check_dir(path):
         os.makedirs(path)
 
 
-def preprocess(scan):
-    vol = scan.to_volume()
-    nods = scan.cluster_annotations()
+def create_image_patch(id, i, vol, anns, pad, mask, image):
+    cmask, cbbox = consensus(  # type: ignore
+        anns, clevel=0.5, pad=[(pad, pad), (pad, pad), (0, 0)], ret_masks=False
+    )
+    k = int(0.5 * (cbbox[2].stop - cbbox[2].start))
+    arr = vol[cbbox][:, :, k].astype(np.float32)
+
+    save_image(
+        cmask[:, :, k].astype(np.float32),
+        os.path.join(mask, f"{id:04d}-{i:02d}.png"),
+        interpolator=sitk.sitkNearestNeighbor,
+    )
+    save_image(arr, os.path.join(image, f"{id:04d}-{i:02d}.png"))
+
+
+def preprocess(scan, coll: deque, progress):
+    with NoP():
+        vol = scan.to_volume()
+        nods = scan.cluster_annotations()
 
     check_dir(IMG_MASK)
     check_dir(IMG)
@@ -69,32 +106,28 @@ def preprocess(scan):
 
     for i, anns in enumerate(nods):
         # IMAGES
-        cmask, cbbox = consensus(  # type: ignore
-            anns, clevel=0.5, pad=[(512, 512), (512, 512), (0, 0)], ret_masks=False
-        )
-        k = int(0.5 * (cbbox[2].stop - cbbox[2].start))
-        arr = vol[cbbox][:, :, k].astype(np.float32)
-
-        save_image(
-            cmask[:, :, k].astype(np.float32),
-            os.path.join(IMG_MASK, f"{scan.id:04d}-{i:02d}.png"),
-            interpolator=sitk.sitkNearestNeighbor,
-        )
-        save_image(arr, os.path.join(IMG, f"{scan.id:04d}-{i:02d}.png"))
+        create_image_patch(scan.id, i, vol, anns, pad=512, mask=IMG_MASK, image=IMG)
 
         # PATCHES
-        cmask, cbbox = consensus(  # type: ignore
-            anns, clevel=0.5, pad=[(48, 48), (48, 48), (0, 0)], ret_masks=False
-        )
-        k = int(0.5 * (cbbox[2].stop - cbbox[2].start))
-        arr = vol[cbbox][:, :, k].astype(np.float32)
+        create_image_patch(scan.id, i, vol, anns, pad=48, mask=PATCH_MASK, image=PATCH)
 
-        save_image(
-            cmask[:, :, k].astype(np.float32),
-            os.path.join(PATCH_MASK, f"{scan.id:04d}-{i:02d}.png"),
-            interpolator=sitk.sitkNearestNeighbor,
-        )
-        save_image(arr, os.path.join(PATCH, f"{scan.id:04d}-{i:02d}.png"))
+        # LABELS
+        list = []
+        for ann in anns:
+            list.append(ann.malignancy)
+
+        labels = {}
+        malignancy = median_high(list)
+        labels["malignancy"] = malignancy
+        if malignancy > 3:
+            labels["cancer"] = "Yes"
+        elif malignancy < 3:
+            labels["cancer"] = "No"
+        else:
+            labels["cancer"] = "Ambiguous"
+
+        coll.append({"id": f"{scan.id:04d}-{i:02d}", "label": labels})
+    progress.update()
 
 
 def create_dataset():
@@ -102,6 +135,7 @@ def create_dataset():
     patch_mask_paths = glob.glob(os.path.join(PATCH_MASK, "*.png"))
     image_paths = glob.glob(os.path.join(IMG, "*.png"))
     image_mask_paths = glob.glob(os.path.join(IMG_MASK, "*.png"))
+    labels = pd.read_csv("./data/labels.csv")
 
     dataset = Dataset.from_dict(
         {
@@ -109,6 +143,7 @@ def create_dataset():
             "image_mask": sorted(image_mask_paths),
             "patch": sorted(patch_paths),
             "patch_mask": sorted(patch_mask_paths),
+            "malignancy": [el["malignancy"] for _, el in labels.iterrows()],
         }
     )
 
@@ -119,18 +154,31 @@ def create_dataset():
 
     login(token=os.environ.get("HF_TOKEN"))
     dataset.push_to_hub("lidc-idri-segmentation")
-    print("Done")
 
 
 def main():
     scans = pl.query(pl.Scan).all()
+    count = pl.query(pl.Scan).count()
+
+    progress = tqdm(total=count)
 
     results = []
-    with c.ThreadPoolExecutor(max_workers=12) as executor:
+    coll = deque()
+    with c.ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
         for scan in scans:
-            result = executor.submit(preprocess, scan)
+            result = executor.submit(preprocess, scan, coll, progress)
 
         for result in results:
             result.join()
 
-    print("Done")
+    labels = [
+        {
+            "id": el["id"],
+            "malignancy": el["label"]["malignancy"],
+            "cancer": el["label"]["cancer"],
+        }
+        for el in sorted(list(coll), key=lambda x: x["id"])
+    ]
+
+    df = pd.DataFrame(data=labels)
+    df.to_csv("./data/labels.csv")
--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -1,8 +1,17 @@
+from sqlalchemy.testing.plugin.plugin_base import logging
 import pytest
+import logging
 import pylidc as pl
 import preprocess as p
+
+from collections import deque
+
+
+LOG = logging.getLogger(__name__)
 
 
 def test_preprocess():
     scan = pl.query(pl.Scan).first()
-    p.preprocess(scan)
+    coll = deque()
+    p.preprocess(scan, coll)
+    LOG.info(coll)
